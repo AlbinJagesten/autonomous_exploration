3d89e1ca2bc16b9c4def07274c6842a24f6a3349
Albin <albin.jagesten@gmail.com>
2019-03-02 10:03:29 -0800

    Added Dijstra algorithm

--- scripts/trajectory_planner.py
+++ scripts/trajectory_planner.py
@@ -3,6 +3,7 @@
 import rospy
 import numpy as np
 import tf
+import operator
 
 from nav_msgs.msg import OccupancyGrid
 
@@ -28,40 +29,85 @@ class TrajectoryPlanner:
 
     def find_trajectory(self):
 
+        """
+        Return a trajectory i.e. a list of map coordinates
+        """
+
         translation,_ = self.trans_listener.lookupTransform("/map", "/base_footprint", rospy.Time(0))
         x = int(np.round(translation[0] / self.metadata.resolution) + self.w / 2)
         y = int(np.round(translation[1] / self.metadata.resolution) + self.h / 2)
-        
+
+        #TODO: Add Dijkstra Algorithm to find Unkown location in costmap
+
+        visited = np.zeros(self.costmap.shape)
+        visited[y,x] = 1
+
+        to_explore = self.add_neighbors(visited, Node(x,y,0,None))
+        to_explore.sort(key=operator.attrgetter('cost'))
+
+        while to_explore:   
+            next_node = to_explore.pop(0)
+            if next_node.cost == -1:
+                self.get_trajectory(next_node)
+                break
+            
+            to_explore = to_explore + self.add_neighbors(visited, next_node)
+            to_explore.sort(key=operator.attrgetter('cost'))
+
+
+
+    def get_trajectory(self, node):
+        print("Done")
+        print(node.x - self.w / 2)
+        print(node.y - self.h / 2)
+
+
+    def add_neighbors(self,visited, parent):
+        x = parent.x
+        y = parent.y
+        cost = parent.cost
+        neighbors = []
+        neighbor_grid = [(-1,1), (0,1), (1,1), (-1,0), (0,0), (1,0), (-1,-1), (0,-1), (1,-1)]
+
+        for idx in neighbor_grid:
+            new_x = x + idx[0]
+            new_y = y + idx[1]
+            if self.valid_pos(new_x, new_y, visited):
+                visited[new_y, new_x] = 1
+                neighbors.append(self.new_node(new_x, new_y, cost, parent))
+
+        return neighbors
+
+
+    def valid_pos(self, x, y, visited):
+        try:
+            return visited[y,x] == 0 and self.costmap[y,x]<99
+        except IndexError:
+            return False
+
+
+    def new_node(self, x, y, cost, parent):
+        if self.costmap[y,x] == 0:
+            return Node(x,y,-1,parent)
+        return Node(x,y,cost+1,parent)
+
 
     def run(self):
         while not rospy.is_shutdown():
             pass
 
 
-    class Node:
+class Node:
 
-
-        def __init__(self, x,y, parent, cost):
-            self.x = x
-            self.y = y
-            self.cost = cost
-            self.parent = parent
-
-
-        def get_cost(self):
-            return self.cost
-
-
-        def get_parent(self):
-            return self.parent
-
-        
-        def get_pos(self):
-            return self.x, self.y
+    def __init__(self, x,y, cost, parent):
+        self.x = x
+        self.y = y
+        self.cost = cost
+        self.parent = parent
 
 
 if __name__ == "__main__":
     traj_plan = TrajectoryPlanner()
-    #traj_plan.run()
+    traj_plan.run()
     
     
